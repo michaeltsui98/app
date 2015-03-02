3bed0a87a4ee341e7cb13484956e5b15932c7696
michael <michaeltsui98@qq.com>
2015-03-02 22:27:34 +0800

    edit

--- Models/AppChannel.php
+++ Models/AppChannel.php
@@ -5,7 +5,7 @@
  * @author michael
  * @version 1.0 2013/8/12 14:17:34 
  */
-class Models_Audit extends Cola_Model
+class Models_AppChannel extends Cola_Model
 {
 
     protected  $_table = 'app_channel';
